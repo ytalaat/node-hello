648d8ba34e2f7ac1fefb2fc53be9f76b85a30ff2
Sabry Tarek <starek@axisapp.com>
2022-08-18 11:23:53 +0200

    Intial commit at staging branch

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const http = require('http');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node!\n'
+  const msg = 'Hello Node from Staging\n'
   res.end(msg);
 });
 
