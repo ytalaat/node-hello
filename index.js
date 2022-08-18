ff95c161ed976fc96143fde0b7f177a33977e00f
Sabry Tarek <starek@axisapp.com>
2022-08-18 11:29:02 +0200

    Initial commit at test branch

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const http = require('http');
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 9000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node from Staging\n'
+  const msg = 'Hello Node from Test\n'
   res.end(msg);
 });
 
